c5a0fdb8e3ca221f59c028b0960ac6a0ec7a1b2e
Ryan <rsatterlee@precisiongovernors.com>
2014-02-21 10:42:14 -0600

    Fix 4 heroku

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os,sys
 import atexit
 import cherrypy
-try:
-	cherrypy.config.update({'environment':'embedded'})
+from cherrypy.process import servers
 
+try:
+	def fake_wait_for_occupied_port(host, port): return
+	servers.wait_for_occupied_port = fake_wait_for_occupied_port
+
+	cherrypy.config.update({'environment':'embedded'})
+	port = int(os.environ.get("PORT", 5000))
+	print port
 	#if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
 		#cherrypy.engine.start(blocking=False)
 		#atexit.register(cherrypy.engine.stop)
@@ -11,7 +17,7 @@ try:
 	PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
 
 	class Root(object): pass
-
+	cherrypy.config.update({'server.socket_port': port,})
 	cherrypy.tree.mount(Root(), '/', config={
 			'/': {
 					'tools.staticdir.on': True,
@@ -19,7 +25,7 @@ try:
 					'tools.staticdir.index': 'index.html',
 				},
 		})
-
+	
 	cherrypy.engine.start()
 	cherrypy.engine.block()
 	#application = cherrypy.Application(Root(), script_name='', config=None)
