59324ea11e00de888e434703b96c127844d4925a
Ryan <rsatterlee@precisiongovernors.com>
2014-02-21 10:33:14 -0600

    Fix 4 heroku

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import os,sys
 import atexit
 import cherrypy
+try:
+	cherrypy.config.update({'environment':'embedded'})
 
-cherrypy.config.update({'environment':'embedded'})
+	#if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
+		#cherrypy.engine.start(blocking=False)
+		#atexit.register(cherrypy.engine.stop)
+		
+	PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
 
-if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
-    cherrypy.engine.start(blocking=False)
-    atexit.register(cherrypy.engine.stop)
-    
-PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
+	class Root(object): pass
 
-class Root(object): pass
+	cherrypy.tree.mount(Root(), '/', config={
+			'/': {
+					'tools.staticdir.on': True,
+					'tools.staticdir.dir': PATH+'/sb-admin-v2',
+					'tools.staticdir.index': 'index.html',
+				},
+		})
 
-cherrypy.tree.mount(Root(), '/', config={
-        '/': {
-                'tools.staticdir.on': True,
-                'tools.staticdir.dir': PATH+'/sb-admin-v2',
-                'tools.staticdir.index': 'index.html',
-            },
-    })
-
-cherrypy.engine.start()
-cherrypy.engine.block()
-#application = cherrypy.Application(Root(), script_name='', config=None)
+	cherrypy.engine.start()
+	cherrypy.engine.block()
+	#application = cherrypy.Application(Root(), script_name='', config=None)
+except Exception, e:
+	print e
