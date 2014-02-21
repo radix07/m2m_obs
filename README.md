bf9feada957f705f7c5fb7f5be19b83c2e747a1c
Ryan <rsatterlee@precisiongovernors.com>
2014-02-21 12:37:01 -0600

    Chart add

---
+++ README.md
@@ -0,0 +1,10 @@
+Flask framework hosted on Heroku
+
+heroku login
+git remote add heroku git@heroku.com:pgsicom.git
+git remote add origin https://github.com/radix07/pgsicom.git
+(git remote -v) -test remotes
+git add *
+git commit -m ""
+git push heroku master (-f)
+heroku open
--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask
 from flask import render_template
+from flask import abort, redirect, url_for
 import os
 from random import choice
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
+def raw_index():
+    app.logger.debug("raw index")
+    return redirect(url_for('index'))
+
 @app.route('/index.html')
 def index():
+    app.logger.debug("index")
     salutation = choice(salutations)
     return render_template('index.html', salutation=salutation)
+
+@app.route('/flot.html')
+def flot():
+    app.logger.debug("chart")
+    salutation = choice(salutations)
+    return render_template('flot.html', salutation=salutation)
 
 salutations = [
     'TEST PG SICOM',]
@@ -19,7 +30,6 @@ salutations = [
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-
     if port == 5000:
         app.debug = True
 
