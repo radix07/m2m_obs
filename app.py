cdcde538fcdc1fed287b1729fb96b915012ed738
Ryan <rsatterlee@precisiongovernors.com>
2014-02-24 10:37:46 -0600

    DB stuff

--- app.py
+++ app.py
@@ -1,25 +1,57 @@
-from flask import Flask
-from flask import render_template
-from flask import abort, redirect, url_for
+from flask import Flask,render_template,abort, redirect, url_for
 import os
 from random import choice
 import httplib
 import base64
+import psycopg2
+import urlparse
 
 username = "pgSatterlee" # enter your username
-password = "Pgecs-2322" # enter your password 
+password = "Pgecs-2322" # enter your password
 
+
+
+#Datbase information for Heroku postgres
+#username: leqslquceprcjb
+#password: bTO0DubrLXbT7fdVjIf-c-nDvI
+#port: 5432
+#database: d9rfmhttltdj9c
+#host: ec2-54-197-227-238.compute-1.amazonaws.com
+#postgres://leqslquceprcjb:bTO0DubrLXbT7fdVjIf-c-nDvI@ec2-54-197-227-238.compute-1.amazonaws.com:5432/d9rfmhttltdj9c
 
 app = Flask(__name__)
+try:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+except:
+    print "local dev"
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres://test.db')
+
+##Postgres
+'''
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+'''
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def raw_index():
     app.logger.debug("raw index")
     return redirect(url_for('index'))
 
-@app.route('/testEtherios')
+@app.route('/testEtherios.xml')
 def etherios():
     auth = base64.encodestring("%s:%s"%(username,password))[:-1]
+    
     webservice = httplib.HTTP("login.etherios.com",80)
     webservice.putrequest("GET", "/ws/DataPoint/0000000-00000000-00042dFF-FF0418fb/EngineSpeedFloat")
     webservice.putheader("Authorization", "Basic %s"%auth)
@@ -32,16 +64,16 @@ def etherios():
 @app.route('/index.html')
 def index():
     app.logger.debug("index")
-    salutation = choice(salutations)
-    return render_template('index.html', salutation=salutation, resp="TEST")
+    #salutation = choice(salutations)
+    return render_template('index.html', salutation=testStringData, resp="TEST")
 
 @app.route('/flot.html')
 def flot():
     app.logger.debug("float")
     salutation = choice(salutations)
-    return render_template('flot.html', salutation=salutation)
+    return render_template('flot.html', salutation=testStringData)
 
-salutations = [
+testStringData = [
     'TEST PG SICOM',]
 
 
--- testFlask.py
+++ testFlask.py
@@ -11,6 +11,38 @@ import base64
 auth = base64.encodestring("%s:%s"%(username,password))[:-1]
 webservice = httplib.HTTP("login.etherios.com",80)
 
+message = """<sci_request version="1.0"> 
+  <send_message cache="false"> 
+    <targets> 
+      <device id="00000000-00000000-00042dFF-FF0418fb"/>
+    </targets> 
+    <rci_request version="1.1"> 
+      <query_setting/>
+    </rci_request>
+  </send_message>
+</sci_request>
+"""
+webservice = httplib.HTTP("login.etherios.com",80)
+
+# to what URL to send the request with a given HTTP method
+webservice.putrequest("POST", "/ws/sci")
+
+# add the authorization string into the HTTP header
+webservice.putheader("Authorization", "Basic %s"%auth)
+webservice.putheader("Content-type", "text/xml; charset=\"UTF-8\"")
+webservice.putheader("Content-length", "%d" % len(message))
+webservice.endheaders()
+webservice.send(message)
+
+# get the response
+statuscode, statusmessage, header = webservice.getreply()
+response_body = webservice.getfile().read()
+
+# print the output to standard out
+print (statuscode, statusmessage)
+print response_body
+
+'''
 # to what URL to send the request with a given HTTP method
 webservice.putrequest("GET", "/ws/DataPoint/0000000-00000000-00042dFF-FF0418fb/EngineSpeedFloat")
 #0000000-00000000-00042dFF-FF0418fb/EngineSpeedFloat
@@ -26,3 +58,5 @@ response_body = webservice.getfile().read()
 # print the output to standard out
 print (statuscode, statusmessage)
 print response_body
+
+'''
