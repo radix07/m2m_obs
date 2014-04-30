8f12d4b45a74d004168ac65e49afcb8dee585957
Ryan <rsatterlee@precisiongovernors.com>
2014-04-30 14:22:50 -0500

    formatting

--- config.py
+++ config.py
@@ -13,36 +13,9 @@ else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_RECORD_QUERIES = True
-WHOOSH_BASE = os.path.join(basedir, 'search.db')
-
-# Whoosh does not work on Heroku
-#WHOOSH_ENABLED = os.environ.get('HEROKU') is None
 
 # slow database query threshold (in seconds)
 DATABASE_QUERY_TIMEOUT = 0.5
 
-# email server
-'''
-MAIL_SERVER = '' # your mailserver
-MAIL_PORT = 25
-MAIL_USE_TLS = False
-MAIL_USE_SSL = False
-MAIL_USERNAME = 'you'
-MAIL_PASSWORD = 'your-password'
-'''
-# available languages
-LANGUAGES = {
-    'en': 'English',
-    'es': 'Español'
-}
-
-# microsoft translation service
-MS_TRANSLATOR_CLIENT_ID = '' # enter your MS translator app id here
-MS_TRANSLATOR_CLIENT_SECRET = '' # enter your MS translator app secret here
-
 # administrator list
 ADMINS = ['you@example.com']
-
-# pagination
-POSTS_PER_PAGE = 50
-MAX_SEARCH_RESULTS = 50
